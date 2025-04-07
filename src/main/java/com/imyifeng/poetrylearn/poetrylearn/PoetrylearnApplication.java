976178f1a48db5a1d3fe26d4123cb4cf92f1de83
ImYiFeng <70053196+ImYiFeng@users.noreply.github.com>
2025-04-07 18:22:56 +0800

    Build the project

---
+++ src/main/java/com/imyifeng/poetrylearn/poetrylearn/PoetrylearnApplication.java
@@ -0,0 +1,13 @@
+package com.imyifeng.poetrylearn.poetrylearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoetrylearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoetrylearnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/imyifeng/poetrylearn/poetrylearn/PoetrylearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imyifeng.poetrylearn.poetrylearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoetrylearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
